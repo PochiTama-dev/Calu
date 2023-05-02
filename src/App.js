b4fce0d91fb92e1dfb5f9bfb2df557915977cb19
GonzaEnrique27 <black3vampire@gmail.com>
2023-05-02 20:55:25 -0300

    Background Clouds

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import  Home from './Components/Home/Home';
 import  About  from './Components/About/About';
 import  Contact  from './Components/Contact/Contact';
 import  Services  from './Components/Services/Services';
-
+import './App.css'
 function App() {
   return (
     <Router>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
-import './home.css'
+import React from "react";
 
-const Home = () => {
+function Home() {
+  const cloudLayerLeftStyle = {
+    position: "fixed",
+    left: "0",
+    top: "0",
+    zIndex: "-1",
+    animation: "slide-left 30s linear infinite",
+  };
+
+  const cloudLayerRightStyle = {
+    position: "fixed",
+    right: "0",
+    top: "0",
+    zIndex: "-1",
+    animation: "slide-right 40s linear infinite",
+  };
+
+  const clouds = Array.from({ length: 40 }, (_, i) => {
+    const xPos = Math.random() * window.innerWidth;
+    const yPos = Math.random() * window.innerHeight;
+    const minSize = 1000;
+    const maxSize = 1250;
+    const size = Math.floor(
+      Math.random() * (maxSize - minSize + 1) + minSize
+    );
+    const aspectRatio = 0.75;
+
+    const width = Math.floor(size * aspectRatio);
+    const height = size;
+
+    const slideCloudsKeyframes = `@keyframes slide-clouds-${i} {
+      0% { left: ${xPos - window.innerWidth}px; }
+      100% { left: ${xPos + window.innerWidth}px; }
+    }`;
+
+    return (
+      <>
+        <style>{slideCloudsKeyframes}</style>
+        <img
+          key={i}
+          style={{
+            position: "absolute",
+            left: `${xPos}px`,
+            top: `${yPos}px`,
+            width: `${width}px`,
+            height: `${height}px`,
+            animation: `slide-clouds-${i} 60s linear infinite`,
+          }}
+          src="./nube1.png"
+          alt="cloud"
+        />
+      </>
+    );
+  });
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <div style={cloudLayerLeftStyle}>{clouds}</div>
+      <div style={cloudLayerRightStyle}>{clouds}</div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
