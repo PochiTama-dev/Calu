a4b37d37c4d02aa9a1e904b3cd75f393ee8c257d
PabloB <braguepablo@gmail.com>
2023-05-16 15:50:13 -0300

    navbar con logo y gradiante

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import "./header.css"
+import miImagen from "../Header/logocalu.png"
+
 
 export const Header = () => {
   return (
-    <div>header</div>
+    <header className="navBar">
+        <div>
+        <img className='logoCalu' src={miImagen} alt="Logo Calu" />
+        </div>
+    </header>
   )
+
+   
+
+    
 }
