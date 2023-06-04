444bf0bf9218b7ec357a17a61f183942ff67cb11
facuzero <facuzero_95@hotmail.com>
2023-06-03 22:41:07 -0300

    Modifico estilos para que sea responsivo

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import "./header.css"
-import miImagen from "../Header/logocalu.png"
-
+import React, { useState } from 'react';
+import './header.css';
+import miImagen from '../Header/logocalu.png';
+import { Link } from 'react-router-dom';
+import bars from '../../images/bars-solid.svg';
 
 export const Header = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
   return (
-    <header className="navBar">
-        <div>
-        <img className='logoCalu' src={miImagen} alt="Logo Calu" />
+    <header className='navBar'>
+      <nav>
+        <Link to={'/'}>
+          <img className='logoCalu' src={miImagen} alt='Logo Calu' />
+        </Link>
+        <div className={showLinks ? 'link show ' : 'link'}>
+          <Link to={'/blog'}> BLOG </Link>
+          <Link to={'/products'}> Productos </Link>
         </div>
+        <button className='burger' onClick={handleLinks}>
+          <img src={bars} alt='logoBurger' />
+        </button>
+      </nav>
     </header>
-  )
-
-   
-
-    
-}
+  );
+};
