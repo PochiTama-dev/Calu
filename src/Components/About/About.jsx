8d7f77cab45f321c39d15feeab543a0acfc23f2b
fakumetal <114892011+fakumetal@users.noreply.github.com>
2023-06-11 13:01:36 -0300

    Cambio de clases y estilos

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,19 +4,19 @@ import "./about.css";
 const About = () => {
   return (
     <div className="about_container">
-      <h1 className="title-first-quienes-somos">¿QUIENES SOMOS?</h1>
-      <h2 className="sub-first">SOMOS UN EQUIPO</h2>
-      <h2 className="sub-second">APASIONADO POR LA TRANSFORMACION DIGITAL</h2>
-      <p className="text-first">
+      <h1 className="title_about">¿QUIENES SOMOS?</h1>
+      <h2 className="sub_1">SOMOS UN EQUIPO</h2>
+      <h2 className="sub_2">APASIONADO POR LA TRANSFORMACION DIGITAL</h2>
+      <p>
         Compartimos la pasión por nuestro trabajo y la ilusión por cambiar el
         mundo emprendedor.
       </p>
-      <p className="text-first">
+      <p>
         Potenciamos la identidad digital de tu marca acompañándote en el
         proceso, escuchándote y aconsejándote para que tu experiencia y la de tu
         público sea única.
       </p>
-      <p className="text-last">
+      <p className="text_last">
         !DESDE EL MOMENTO CERO NOS COMPROMETEMOS CON TU PROPÓSITO!
       </p>
     </div>
