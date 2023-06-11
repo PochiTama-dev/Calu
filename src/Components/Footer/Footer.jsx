551dddddca65796d8a7dfa4d57c6c17ac9914225
fakumetal <114892011+fakumetal@users.noreply.github.com>
2023-06-11 13:09:10 -0300

    Se agregan servicios faltantes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -19,11 +19,14 @@ const Footer = () => {
       <div className="servicios">
         <h2>SERVICIOS</h2>
         <ul>
-          <li>Consultoría Estatégica</li>
+          <li>Consultoría Estratégica</li>
+          <li>Gestión de Redes Sociales</li>
           <li>Publicidad Digital</li>
-          <li>Desarrolo Web</li>
-          <li>Manejo de Redes</li>
-          <li>Email Marketing</li>
+          <li>E-mail Marketings</li>
+          <li>Book Institucional</li>
+          <li>Creación de Contenido</li>
+          <li>Modelo de Negocio</li>
+          <li>Desarrollo Web</li>
         </ul>
       </div>
 
