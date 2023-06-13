3985b2e3d781294526b1ceda0c9a8b234417c5ff
Facundo Journade <faku.metal@gmail.com>
2023-06-12 21:36:47 -0300

    Add News

---
+++ src/Components/News/News.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./news.css";
+import Card_news from "./Card_news/Card_news";
+const News = () => {
+  return (
+    <div className="novedades_container">
+      <h1 className="title_novedades">NOVEDADES</h1>
+      <p>No te pierdas los úlimos contenidos de nuestro blog.</p>
+      <div className="cards_novedades">
+        <Card_news
+          image={
+            <img className="icons_novedades" src={""} alt="" width="50%" />
+          }
+          title={"TENDENCIAS 2023"}
+          btn={
+            <a
+              target="_blank"
+              className="
+                  button_novedades"
+              href=" "
+            >
+              Ver más
+            </a>
+          }
+        ></Card_news>
+        <Card_news
+          image={
+            <img className="icons_novedades" src={""} alt="" width="50%" />
+          }
+          title={"TENDENCIAS 2023"}
+          btn={
+            <a
+              target="_blank"
+              className="
+                  button_novedades"
+              href=" "
+            >
+              Ver más
+            </a>
+          }
+        ></Card_news>
+        <Card_news
+          image={
+            <img className="icons_novedades" src={""} alt="" width="50%" />
+          }
+          title={"TENDENCIAS 2023"}
+          btn={
+            <a
+              target="_blank"
+              className="
+                  button_novedades"
+              href=" "
+            >
+              Ver más
+            </a>
+          }
+        ></Card_news>
+      </div>
+    </div>
+  );
+};
+
+export default News;
