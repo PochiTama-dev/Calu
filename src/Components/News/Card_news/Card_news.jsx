4dfb975505027a2e3c783fb2fdecc006686dee0e
Facundo Journade <faku.metal@gmail.com>
2023-06-13 09:00:16 -0300

    Add cambios css mobile

--- src/Components/News/Card_news/Card_news.jsx
+++ src/Components/News/Card_news/Card_news.jsx
@@ -5,10 +5,12 @@ import "./card_news.css";
 const Card_news = ({ image, title, btn }) => {
   return (
     <div className="card_news">
-      <div className="card_f">
-        <div className="arrow">
-          <img src={arrow_R} alt=" " className="arrow_R" />
-          <img src={arrow_R} alt=" " className="arrow_R" />
+      <div className="card_cont">
+        <div className="card_f">
+          <div className="arrow">
+            <img src={arrow_R} alt=" " className="arrow_R" />
+            <img src={arrow_R} alt=" " className="arrow_R" />
+          </div>
         </div>
         <div className="image">{image}</div>
       </div>
