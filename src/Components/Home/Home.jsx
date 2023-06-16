c992f47e6db5a2ba01e3262ecaf6ddff9e868bf8
facuzero <facuzero_95@hotmail.com>
2023-06-16 16:28:31 -0300

    utilizo el componente creado en el Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,17 +3,13 @@ import { useSwipeable } from 'react-swipeable';
 import 'animate.css/animate.min.css';
 import './home.css';
 import { Header } from '../Header/header';
-import icono from '../Home/icon_eye.svg';
-import icono2 from '../Home/🦆 icon _key_.svg';
-import icono3 from '../Home/🦆 icon _pie chart_.svg';
 import Footer from '../Footer/Footer';
 import Portfolio from '../Portfolio/Portfolio';
 import About from '../About/About';
 import News from '../News/News';
-import Slider from '../Portfolio/Slider/Slider';
-import Card from '../Portfolio/Card/Card';
 import LogoContact from '../Logo-contact/LogoContact';
 import Onboarding from '../Onboarding/Onboarding';
+import OurServices from '../OurServices/OurServices';
 
 const Home = () => {
   const numSections = 5;
@@ -56,59 +52,7 @@ const Home = () => {
         style={{ height: '110vh', transition: 'transform 0.5s ease-in-out' }}
         className={`section-2 ${currentSection === 1 ? 'animate__animated animate__fadeIn' : ''}`}
       >
-        <h1 className='title-first-nuestros-servicios'>NUESTROS SERVICIOS</h1>
-        <p className='text-second'>
-          Contamos con una amplia gama de servicios diseñados a la medida de cada emprendimiento que
-          quie ra tener éxito en el mundo digital.
-        </p>
-        <div className='ctn-servicios'>
-          <div className='slider'>
-            <Slider>
-              <div className='nuestros-servicios'>
-                <Card
-                  image={<img className='icono-servicios' src={icono3} alt='icono pay' />}
-                  title={<p className='text-icono-servicios'>CONSULTORIA ESTRATEGICA</p>}
-                ></Card>
-                <p className='text-transition'>
-                  Identificamos oportunidades de mejora y te ayudamos a hacer crecer tu negocio
-                  ¿Sentís que tu marca no despega? En CALU analizamos la situación actual y
-                  diseñamos una estrategia de comunicación para definir la identidad digital de tu
-                  marca y mejorar tus resultados. Nuestro modelo de trabajo se basa en la cercanía
-                  con las personas para sumar valor y lograr que sus negocios despeguen. ¡Empecemos
-                  a trabajar en equipo!
-                </p>
-              </div>
-              <div className='nuestros-servicios'>
-                <Card
-                  image={<img className='icono-servicios' src={icono} alt='icono ojo' />}
-                  title={<p className='text-icono-servicios'>GESTIÓN DE REDES SOCIALES</p>}
-                ></Card>
-                <p className='text-transition'>
-                  Potenciamos tus Redes Sociales y generamos una comunidad que se identifique con tu
-                  marca Te ayudamos a identificar tu público ideal, definimos una estrategia y
-                  armamos un plan de contenidos para mejorar la presencia digital de tu marca, darle
-                  más alcance y aumentar tus ventas. ¡Dale vida a tus redes y destacate de la
-                  competencia!
-                </p>
-              </div>
-              <div className='nuestros-servicios'>
-                <Card
-                  image={<img className='icono-servicios' src={icono2} alt='icono llave' />}
-                  title={<p className='text-icono-servicios'>PUBLICIDAD DIGITAL</p>}
-                ></Card>
-                <p className='text-transition'>
-                  Escalamos tu negocio y maximizamos la visibilidad de tu marca con publicidades
-                  efectivas Creamos tus campañas de publicidad, en Google ADS, Meta ADS, LinkedIn
-                  Ads y TikTok Ads con anuncios altamente personalizados y segmentados, para
-                  dirigirlos a un público que realmente se interese en tus productos o servicios.
-                  Medimos las métricas de las campañas y realizamos los ajustes necesarios para
-                  obtener mejores resultados. ¡Incrementá tu presencia online, dejá tu huella y que
-                  más personas te conozcan!
-                </p>
-              </div>
-            </Slider>
-          </div>
-        </div>
+        <OurServices />
       </section>
       <section
         style={{ height: '110vh', transition: 'transform 0.5s ease-in-out' }}
