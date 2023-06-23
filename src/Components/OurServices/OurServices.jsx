25ecc4e98cbbd5db3a5a38f81b74cfa036d41c6f
facuzero <facuzero_95@hotmail.com>
2023-06-23 12:11:43 -0300

    agrego primeros parrafos a cada tajerta

--- src/Components/OurServices/OurServices.jsx
+++ src/Components/OurServices/OurServices.jsx
@@ -26,6 +26,11 @@ const OurServices = () => {
                   </a>
                 }
               ></Card>
+              <p className='paragraph'>
+                ¿Sentís que tu marca no despega? En CALU analizamos la situación actual y diseñamos
+                una estrategia de comunicación para definir la identidad digital de tu marca y
+                mejorar tus resultados.
+              </p>
             </div>
             <div className='nuestros-servicios'>
               <Card
@@ -37,6 +42,11 @@ const OurServices = () => {
                   </a>
                 }
               ></Card>
+              <p className='paragraph'>
+                Te ayudamos a identificar tu público ideal, definimos una estrategia y armamos un
+                plan de contenidos para mejorar la presencia digital de tu marca, darle más alcance
+                y aumentar tus ventas.
+              </p>
             </div>
             <div className='nuestros-servicios'>
               <Card
@@ -48,6 +58,11 @@ const OurServices = () => {
                   </a>
                 }
               ></Card>
+              <p className='paragraph'>
+                Creamos tus campañas de publicidad, en Google ADS, Meta ADS, LinkedIn Ads y TikTok
+                Ads con anuncios altamente personalizados y segmentados, para dirigirlos a un
+                público que realmente se interese en tus productos o servicios.
+              </p>
             </div>
           </Slider>
         </div>
