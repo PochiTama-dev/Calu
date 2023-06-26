b08412587c07e18b6bf50c73fa31148c7164648e
Pochitama.dev <111319458+PochiTama-dev@users.noreply.github.com>
2023-06-26 16:25:22 -0300

    Revert "Calu 03 seccion nuestros servicios"

--- src/Components/Portfolio/Card/Card.jsx
+++ src/Components/Portfolio/Card/Card.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './card.css';
-const Card = ({ image, title, text, btn }) => {
+import "./card.css";
+const Card = ({ image, title, btn }) => {
   return (
-    <div className='card'>
-      <div className='image'>{image}</div>
+    <div className="card">
+      <div className="image">{image}</div>
 
-      <div className='title'>{title}</div>
+      <div className="title">{title}</div>
 
-      <div className='text'>{text}</div>
-
-      <div className='card_btn'>{btn}</div>
+      <div className="card_btn">{btn}</div>
     </div>
   );
 };
