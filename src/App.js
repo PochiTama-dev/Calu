18604d878c647cf51b447fce9935f019f820de14
GonzaEnrique27 <black3vampire@gmail.com>
2023-06-26 11:19:53 -0300

    Funcionalidad Blog

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
@@ -7,12 +8,19 @@ import Services from './Components/Services/Services';
 import './App.css';
 import Blog from './Components/Blog/Blog';
 import VideoBackgroundComponent from './Components/VideoBackgroundComponent/VideoBackgroundComponent';
+import AdminLogin from './Components/AdminLogin/AdminLogin'
+import CreatePost from './Components/CreatePost/CreatePost';
+
+
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
   return (
     <Router>
       <VideoBackgroundComponent/>
       <Routes>
         <Route exact path='/' element={<Home />} />
+        <Route path='/AdminLogin' element={<AdminLogin setIsAuth={setIsAuth} />} />
+        <Route path='/CreatePost' element={<CreatePost/>}/>
         <Route path='/about' element={<About />} />
         <Route path='/blog' element={<Blog />} />
         <Route path='/contact' element={<Contact />} />
--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,11 +1,64 @@
-import { Header } from '../Header/header';
+import React, { useEffect, useState } from "react";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { auth, db } from "../../firebase-config";
 
-const Blog = () => {
+function Blog({ isAuth }) {
+  const [postLists, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+  };
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  }, [deletePost]);
+
   return (
-    <>
-      <Header />
-      <div>blog</div>
-    </>
+    <div className="BlogPage">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1>{post.title}</h1>
+              </div>
+              <div className="deletePost">
+                {isAuth && post.author && post.author.id === auth.currentUser.uid && (
+                  <>
+                    <button
+                      onClick={() => {
+                        deletePost(post.id);
+                      }}
+                      className="deleteButton"
+                    >
+                      &#128465; Delete
+                    </button>
+                    <button
+                      onClick={() => {
+                        // Lógica para editar el post
+                      }}
+                      className="editButton"
+                    >
+                      &#9998; Edit
+                    </button>
+                  </>
+                )}
+              </div>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            {post.author && <h3>@{post.author.name}</h3>}
+          </div>
+        );
+      })}
+    </div>
   );
-};
+}
+
 export default Blog;
