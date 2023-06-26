0b5ef97805d7ebb1792105a610d9f5f1dd617a57
Pochitama.dev <111319458+PochiTama-dev@users.noreply.github.com>
2023-06-25 21:33:50 -0300

    Revert "Calu 05 footer"

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,66 +8,6 @@ import tk_logo from "./icons/TikTok.png";
 import sp_logo from "./icons/Spotify.png";
 
 const Footer = () => {
-  const [width, setWidth] = React.useState(window.innerWidth);
-  const breakpoint = 560;
-  React.useEffect(() => {
-    const handleResizeWindow = () => setWidth(window.innerWidth);
-
-    window.addEventListener("resize", handleResizeWindow);
-    return () => {
-      window.removeEventListener("resize", handleResizeWindow);
-    };
-  }, []);
-  if (width > breakpoint) {
-    return (
-      <div className="footer_container">
-        <div className="logo_calu">
-          <img src={calu_logo} alt="calu" />
-          <p>TU AGENCIA DE MARKETING DIGITAL</p>
-        </div>
-
-        <div className="servicios">
-          <h1>SERVICIOS</h1>
-
-          <div className="servicios_">
-            <ul>
-              <li>Consultoría Estratégica</li>
-              <li>Gestión de Redes Sociales</li>
-              <li>Publicidad Digital</li>
-              <li>E-mail Marketings</li>
-            </ul>
-            <ul>
-              <li>Book Institucional</li>
-              <li>Creación de Contenido</li>
-              <li>Modelo de Negocio</li>
-              <li>Desarrollo Web</li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="redes">
-          <h2>REDES</h2>
-          <div className="social_icons">
-            <a href="">
-              <img src={ld_logo} alt="linkedin" />
-            </a>
-            <a href="">
-              <img src={fb_logo} alt="facebook" />
-            </a>
-            <a href="">
-              <img src={insta_logo} alt="instagram" />
-            </a>
-            <a href="">
-              <img src={tk_logo} alt="tiktok" />
-            </a>
-            <a href="">
-              <img src={sp_logo} alt="spotify" />
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  }
   return (
     <div className="footer_container">
       <div className="logo_calu">
@@ -76,20 +16,17 @@ const Footer = () => {
       </div>
 
       <div className="servicios">
-        <h1>SERVICIOS</h1>
-
-        <div className="servicios_">
-          <ul>
-            <li>Consultoría Estratégica</li>
-            <li>Gestión de Redes Sociales</li>
-            <li>Publicidad Digital</li>
-            <li>E-mail Marketings</li>
-            <li>Book Institucional</li>
-            <li>Creación de Contenido</li>
-            <li>Modelo de Negocio</li>
-            <li>Desarrollo Web</li>
-          </ul>
-        </div>
+        <h2>SERVICIOS</h2>
+        <ul>
+          <li>Consultoría Estratégica</li>
+          <li>Gestión de Redes Sociales</li>
+          <li>Publicidad Digital</li>
+          <li>E-mail Marketings</li>
+          <li>Book Institucional</li>
+          <li>Creación de Contenido</li>
+          <li>Modelo de Negocio</li>
+          <li>Desarrollo Web</li>
+        </ul>
       </div>
 
       <div className="redes">
