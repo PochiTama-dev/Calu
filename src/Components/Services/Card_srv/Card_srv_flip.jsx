7db93c35f984759aab057b2834c2e1de00f4d275
Pochitama.dev <111319458+PochiTama-dev@users.noreply.github.com>
2023-06-25 21:34:22 -0300

    Revert "Modificado card servicios"

--- src/Components/Services/Card_srv/Card_srv_flip.jsx
+++ src/Components/Services/Card_srv/Card_srv_flip.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./card_srv_flip.css";
+import { useState } from "react";
 
 const Card_srv_flip = ({ image, title, sub, des_1, des_2, des_3 }) => {
   const handleClick = () => {};
@@ -8,27 +9,23 @@ const Card_srv_flip = ({ image, title, sub, des_1, des_2, des_3 }) => {
     <div class="flip-card">
       <div class="flip-card-inner">
         <div class="flip-card-front">
-          <div> {image}</div>
-          <div className="title">{title}</div>
-          <div className="sub">{sub}</div>
-
-          <button className="card_btn" onClick={handleClick}>
-            VER MÁS
-          </button>
+          <div className="flip-card-front_items">
+            <div className="image">{image}</div>
+            <div className="title">{title}</div>
+            <div className="sub">{sub}</div>
+          </div>
+          <div className="card_btn_ctn">
+            <button className="card_btn" onClick={handleClick}>
+              Ver Más
+            </button>
+          </div>
         </div>
-
         <div class="flip-card-back">
-          <div>
-            <p>{des_1}</p>
-          </div>
+          <div>{des_1}</div>
           <br />
-          <div>
-            <p>{des_2}</p>
-          </div>
+          <div>{des_2}</div>
           <br />
-          <div className="des_3">
-            <p>{des_3}</p>
-          </div>
+          <div className="des_3">{des_3}</div>
         </div>
       </div>
     </div>
--- src/Components/Services/constants.js
+++ src/Components/Services/constants.js
@@ -8,96 +8,88 @@ import modelo_negocio from "./icons/modelo_negocio.png";
 import desarrollo_web from "./icons/desarrollo_web.png";
 const servicios = [
   {
-    id: 1,
-    img: <img src={consultoria_estategica} width="210px" />,
-    title: "CONSULTORÍA ESTRATÉGICA DE MARKETING Y COMUNICACIÓN ",
-    sub: "IDENTIFICAMOS OPORTUNIDADES DE MEJORA Y TE AYUDAMOS A HACER CRECER TU NEGOCIO",
+    img: <img src={consultoria_estategica} width="100px" />,
+    title: "Consultoría Estratégica de Marketing Y Comunicación ",
+    sub: "Identificamos oportunidades de mejora y te ayudamos a hacer crecer tu negocio",
     des_1:
       "¿Sentís que tu marca no despega? En CALU analizamos la situación actual y diseñamos una estrategia de comunicación para definir la identidad digital de tu marca y mejorar tus resultados.",
     des_2:
       "Nuestro modelo de trabajo se basa en la cercanía con las personas para sumar valor y lograr que sus negocios despeguen.",
-    des_3: "¡EMPECEMOS A TRABAJAR EN EQUIPO!",
+    des_3: "¡Empecemos a trabajar en equipo!",
   },
 
   {
-    id: 2,
-    img: <img src={gestion_redes} width="210px" />,
-    title: " GESTIÓN DE REDES SOCIALES ",
-    sub: "POTENCIAMOS TUS REDES SOCIALES Y GENERAMOS UNA COMUNIDAD QUE SE IDENTIFIQUE CON TU MARCA",
+    img: <img src={gestion_redes} width="100px" />,
+    title: " Gestión de Redes Sociales ",
+    sub: "Identificamos oportunidades de mejora y te ayudamos a hacer crecer tu negocio",
     des_1:
       "Te ayudamos a identificar tu público ideal, definimos una estrategia y armamos un plan de contenidos para mejorar la presencia digital de tu marca, darle más alcance y aumentar tus ventas. ",
 
-    des_3: "¡DALE VIDA A TUA REDES Y DESTACATE DE LA COMPETENCIA!",
+    des_3: "¡Dale vida a tus redes y destacate de la competencia!",
   },
   {
-    id: 3,
-    img: <img src={publicidad_digital} width="210px" />,
-    title: " PUBLICIDAD DIGITAL ",
-    sub: "ESCALAMOS TU NEGOCIO Y MAXIMIZAMOS LA VISIBILIDAD DE TU MARCA CON PUBLICIDADES EFECTIVAS",
+    img: <img src={publicidad_digital} width="100px" />,
+    title: " Publicidad Digital ",
+    sub: "Escalamos tu negocio y maximizamos la visibilidad de tu marca con publicidades efectivas",
     des_1:
       "Creamos tus campañas de publicidad, en Google ADS, Meta ADS, LinkedIn Ads y TikTok Ads con anuncios altamente personalizados y segmentados, para dirigirlos a un público que realmente se interese en tus productos o servicios.",
     des_2:
       "Medimos las métricas de las campañas y realizamos los ajustes necesarios para obtener mejores resultados.",
     des_3:
-      "¡INCREMENTÁ TU PRECENCIA ONLINE, DEJÁ TU HUELLA Y QUE MÁS PERSONAS TE CONOZCAN!",
+      "¡Incrementá tu presencia online, dejá tu huella y que más personas te conozcan!",
   },
 
   {
-    id: 4,
-    img: <img src={email_marketing} width="210px" />,
-    title: " E-MAIL MARKETING ",
-    sub: "TE AYUDAMOS A MANTENER UNA RELACIÓN MÁS CERCANA CON LAS PERSONAS QUE YA SE INTERESARON CON TU MARCA.",
+    img: <img src={email_marketing} width="100px" />,
+    title: " E-mail Marketing ",
+    sub: "Te ayudamos a mantener una relación más cercana con las personas que ya se interesaron en tu marca.",
     des_1:
       "Creamos campañas personalizadas de email enfocadas en acompañar, nutrir y cuidar la relación de las personas que forman parte de tu base de contactos para guiarlas en el proceso de compra.",
     des_2:
       "Te ayudamos a armar el flujo de emails adecuado para que tus leads se conviertan en ventas seguras.",
-    des_3: "¡SI CREES QUE EL EMAIL PASÓ DE MODA, TE VAS A SORPRENDER!",
+    des_3: "¡Si crees que el email pasó de moda, te vas a sorprender!",
   },
 
   {
-    id: 5,
-    img: <img src={book_institucional} width="210px" />,
-    title: "  BOOK INSTITUCIONAL ",
-    sub: "LE DAMOS VIDA A TU NEGOCIO CON FOTOGRAFÍAS Y VIDEOS DE ALTA CALIDAD",
-    des_1:
-      "Que la calidad de imagen de tus productos, o servicios,  sean tu principal factor de conversión, te ayudamos a transmitir profesionalismo y confianza.",
-
-    des_3:
-      "¡CAPTUREMOS LA ESCENCIA DE TU MARCA DE MANERA CREATIVA Y MARCÁ LA DIFERENCIA!",
-  },
-
-  {
-    id: 6,
-    img: <img src={creación_contenido} width="210px" />,
-    title: "  CREACIÓN DE CONTENIDO ",
-    sub: "CREAMOS INCREÍBLES EXPERIENCIAS CON CONTENIDOS CREATIVOS",
+    img: <img src={book_institucional} width="100px" />,
+    title: "  Book Institucional ",
+    sub: "Que la calidad de imagen de tus productos, o servicios,  sean tu principal factor de conversión, te ayudamos a transmitir profesionalismo y confianza.",
     des_1:
       "Realizamos una investigación de mercado, nos involucramos con tus objetivos  y te ayudamos a definir cuáles serían los contenidos adecuados para alcanzarlos.",
 
-    des_3: "¡DEJÁ TU HUELLA CON CONTENIDOS ALTAMENTE CREATIVOS!",
+    des_3:
+      "¡Capturemos la esencia de tu marca de manera creativa y marcá la diferencia!",
   },
 
   {
-    id: 7,
-    img: <img src={modelo_negocio} width="210px" />,
-    title: " MDOELO DE NEGOCIO ",
-    sub: "LE DAMOS FORMA A TU IDEA Y TE AYUDAMOS A QUE SE CONVIERTA EN REALIDAD ",
+    img: <img src={creación_contenido} width="100px" />,
+    title: "  Creación de Contenido ",
+    sub: "Creamos increíbles experiencias con contenidos creativos",
+    des_1:
+      "Realizamos una investigación de mercado, nos involucramos con tus objetivos  y te ayudamos a definir cuáles serían los contenidos adecuados para alcanzarlos.",
+
+    des_3: "¡Dejá tu huella con contenidos altamente efectivos!",
+  },
+
+  {
+    img: <img src={modelo_negocio} width="100px" />,
+    title: " Modelo de Negocio ",
+    sub: "Le damos forma a tu idea y te ayudamos a que se convierta en realidad ",
     des_1:
       "¿Te cuesta definir el modelo de negocio de tu proyecto? Analicemos en equipo tu idea, definamos de qué manera vas a crear valor, cómo te vas a diferenciar de la competencia y cómo vas a generar ingresos.",
 
-    des_3: "¡DALE VIDA A TU PROYECTO!",
+    des_3: "¡Dale vida a tu proyecto!",
   },
 
   {
-    id: 8,
-    img: <img src={desarrollo_web} width="210px" />,
-    title: "  DESARROLLO WEB ",
-    sub: "DESARROLLAMOS UNA PÁGINA WEB DE ALTO IMPACTO VISUAL QUE PERMITA OFRECER LA MEJOR EXPERIENCIA",
+    img: <img src={desarrollo_web} width="100px" />,
+    title: "  Desarrollo Web ",
+    sub: "Desarrollamos una página web de alto impacto visual que permita ofrecer la mejor experiencia",
     des_1:
       "Partiendo de tus objetivos de negocio, definimos una estrategia clara de desarrollo y diseñamos de manera simple, y amigable, tu sitio web para ofrecer la mejor experiencia.",
 
     des_3:
-      "¡ES MOMENTO DE QUE TENGAS TU PÁGINA WEB ADAPTABLE A TODOS LOS DISPOSITIVOS, Y QUE REFLEJE LA IDENTIDAD DE TU MARCA!",
+      "¡Es momento de que tengas tu página web adaptable a todos los dispositivos, y que refleje la identidad de tu marca!",
   },
 ];
 
