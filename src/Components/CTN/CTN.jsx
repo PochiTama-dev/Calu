6ac6ed726c5328b028edc564640fc869f8934dab
Facundo Journade <faku.metal@gmail.com>
2023-06-26 17:15:52 -0300

    ADD CTN

---
+++ src/Components/CTN/CTN.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ctn.css";
+
+const CTN = () => {
+  return (
+    <div className="CTN_ctn">
+      <div className="CTN_ctn2">
+        <h1 className="CTN_title">¡QUÉ TU NEGOCIO DESPEGE!</h1>
+        <button className="ctn_button"> CONTACTANOS </button>
+        <p>CONTANOS SOBRE TUS PRODUCTOS</p>
+      </div>
+    </div>
+  );
+};
+
+export default CTN;
