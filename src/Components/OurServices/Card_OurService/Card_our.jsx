2a7ecb052ce6890407afb18b17d8f7c62f89f223
Facundo Journade <faku.metal@gmail.com>
2023-07-27 12:54:46 -0300

    crud cards news, services

--- src/Components/OurServices/Card_OurService/Card_our.jsx
+++ src/Components/OurServices/Card_OurService/Card_our.jsx
@@ -4,9 +4,9 @@ import "./card_our.css";
 const Card_our = ({ image, title, des, btn }) => {
   return (
     <div className="card_our">
-      <div className="image">{image}</div>
+      <div className="image_our">{image}</div>
 
-      <div className="title">{title}</div>
+      <div className="title_our">{title}</div>
       <div className="des">{des}</div>
       <div className="card_btn">{btn}</div>
     </div>
