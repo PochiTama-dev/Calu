feb65862ab82fa09a7fc47e8314b5cb4b253556e
GonzaEnrique27 <black3vampire@gmail.com>
2023-08-10 11:11:20 -0300

    funcionamiento guardar email payment

--- src/Components/Payment/Payment.jsx
+++ src/Components/Payment/Payment.jsx
@@ -4,16 +4,19 @@ import Paypal from '../../images/payments/paypal.png'
 import Mercado from '../../images/payments/Mercado-Pago-Logo.png'
 import Stripe from '../../images/payments/2560px-Stripe_Logo,_revised_2016.svg.png'
 import Wise from '../../images/payments/1200x630wa.png'
+import { Header } from "../Header/header";
 
 function PaymentGateway() {
   const handlePayment = (paymentMethod) => {
-    // Aquí puedes agregar la lógica para cada método de pago
     console.log("Seleccionaste el método de pago:", paymentMethod);
   };
 
   return (
-    <div>
-      <h1>Pasarela de Pagos</h1>
+    <div className="payment-content">
+      <Header/>
+      <br /><br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br /><br />
+      <h1 className="title-payment">Pasarela de Pagos</h1>
       <div className="payment-options">
         {/* PayPal */}
         <div className="payment-option" onClick={() => handlePayment("paypal")}>
