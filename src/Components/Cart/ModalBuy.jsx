d2eddd289c535ddee53530f9b500c91f3d0e6637
facuzero <facuzero_95@hotmail.com>
2023-08-11 11:23:57 -0300

    agrego funcionalidad para boton 'descargar archivo' y logica para no mostrar NaN

---
+++ src/Components/Cart/ModalBuy.jsx
@@ -0,0 +1,22 @@
+const ModalBuy = ({ email, setEmail, setIsModalOpen, handleSubmit }) => {
+  return (
+    <div>
+      <div className='emailModal'>
+        <form onSubmit={handleSubmit}>
+          <h3>Ingrese su correo electrónico:</h3>
+          <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <div className='email_btn_ctn'>
+            <button className='email_btn' type='submit'>
+              Continuar
+            </button>
+            <button className='email_btn' onClick={() => setIsModalOpen(false)}>
+              Cancelar
+            </button>
+          </div>
+        </form>
+      </div>
+      <div class='modal-background'></div>
+    </div>
+  );
+};
+export default ModalBuy;
