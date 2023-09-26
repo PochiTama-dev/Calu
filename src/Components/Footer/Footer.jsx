32bfc096cc7b2386aa553695e465361f25f7a2ce
facuzero <facuzero_95@hotmail.com>
2023-09-26 01:31:48 -0300

    agrego boton ver mas

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-import "./footer.css";
-import calu_logo from "../../images/icono_calu.svg";
-import fb_logo from "./icons/Facebook.png";
-import insta_logo from "./icons/Instagram.png";
-import ld_logo from "./icons/Linkedin.png";
-import tk_logo from "./icons/TikTok.png";
-import sp_logo from "./icons/Spotify.png";
-import yt_logo from "./icons/youtube.png";
+import React, { useEffect, useState } from 'react';
+import './footer.css';
+import calu_logo from '../../images/icono_calu.svg';
+import fb_logo from './icons/Facebook.png';
+import insta_logo from './icons/Instagram.png';
+import ld_logo from './icons/Linkedin.png';
+import tk_logo from './icons/TikTok.png';
+import sp_logo from './icons/Spotify.png';
+import yt_logo from './icons/youtube.png';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../firebase-config';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const Footer = () => {
   const [width, setWidth] = React.useState(window.innerWidth);
@@ -14,27 +17,49 @@ const Footer = () => {
   React.useEffect(() => {
     const handleResizeWindow = () => setWidth(window.innerWidth);
 
-    window.addEventListener("resize", handleResizeWindow);
+    window.addEventListener('resize', handleResizeWindow);
     return () => {
-      window.removeEventListener("resize", handleResizeWindow);
+      window.removeEventListener('resize', handleResizeWindow);
     };
   }, []);
+
+  const [servicios, setServicios] = useState([]);
+  const serviciosRef = collection(db, 'servicios');
+  useEffect(() => {
+    const getServices = async () => {
+      const data = await getDocs(serviciosRef);
+      setServicios(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getServices();
+  }, []);
+
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate('/services');
+  };
+
   if (width > breakpoint) {
     return (
-      <div className="footer_container">
-        <div className="footer_background">
-          <div className="footer_elements">
-            <div className="logo_calu">
-              <img src={calu_logo} alt="calu" />
+      <div className='footer_container'>
+        <div className='footer_background'>
+          <div className='footer_elements'>
+            <div className='logo_calu'>
+              <img src={calu_logo} alt='calu' />
               <p>TU AGENCIA DE MARKETING DIGITAL</p>
             </div>
 
-            <div className="servicios">
+            <div className='servicios'>
               <h1>SERVICIOS</h1>
 
-              <div className="servicios_">
+              <div className='servicios_'>
                 <ul>
-                  <li>
+                  {servicios.map((servicio, index) => (
+                    <li key={index}>
+                      <span>{servicio.title}</span>
+                    </li>
+                  ))}
+                  <p onClick={handleNavigate}>Ver más</p>
+                  {/* <li>
                     <span>Consultoría Estratégica</span>
                   </li>
                   <li>
@@ -60,45 +85,37 @@ const Footer = () => {
                   <li>
                     <span>Desarrollo Web</span>
                   </li>
+                */}
                 </ul>
               </div>
             </div>
 
-            <div className="redes">
+            <div className='redes'>
               <h1>REDES</h1>
-              <div className="social_icons">
-                <a
-                  href="https://www.linkedin.com/company/calu-mktdigital/"
-                  target="_blank"
-                >
-                  <img src={ld_logo} alt="linkedin" />
+              <div className='social_icons'>
+                <a href='https://www.linkedin.com/company/calu-mktdigital/' target='_blank'>
+                  <img src={ld_logo} alt='linkedin' />
+                </a>
+                <a href='https://www.facebook.com/profile.php?id=100090611090874' target='_blank'>
+                  <img src={fb_logo} alt='facebook' />
+                </a>
+                <a href='https://www.instagram.com/calu.mktdigital/ ' target='_blank'>
+                  <img src={insta_logo} alt='instagram' />
                 </a>
                 <a
-                  href="https://www.facebook.com/profile.php?id=100090611090874"
-                  target="_blank"
+                  href='https://www.tiktok.com/@calumktdigital?is_from_webapp=1&sender_device=pc'
+                  target='_blank'
                 >
-                  <img src={fb_logo} alt="facebook" />
+                  <img src={tk_logo} alt='tiktok' />
                 </a>
                 <a
-                  href="https://www.instagram.com/calu.mktdigital/ "
-                  target="_blank"
+                  href='https://open.spotify.com/user/31qdqqxbhaph6tshdgkoidsug6ae '
+                  target='_blank'
                 >
-                  <img src={insta_logo} alt="instagram" />
+                  <img src={sp_logo} alt='spotify' />
                 </a>
-                <a
-                  href="https://www.tiktok.com/@calumktdigital?is_from_webapp=1&sender_device=pc"
-                  target="_blank"
-                >
-                  <img src={tk_logo} alt="tiktok" />
-                </a>
-                <a
-                  href="https://open.spotify.com/user/31qdqqxbhaph6tshdgkoidsug6ae "
-                  target="_blank"
-                >
-                  <img src={sp_logo} alt="spotify" />
-                </a>
-                <a href="https://youtube.com/@CaluMktdigital" target="_blank">
-                  <img src={yt_logo} alt="youtube" />
+                <a href='https://youtube.com/@CaluMktdigital' target='_blank'>
+                  <img src={yt_logo} alt='youtube' />
                 </a>
               </div>
             </div>
@@ -108,17 +125,17 @@ const Footer = () => {
     );
   }
   return (
-    <div className="footer_container">
-      <div className="footer_elements_mobile">
-        <div className="logo_calu">
-          <img src={calu_logo} alt="calu" />
+    <div className='footer_container'>
+      <div className='footer_elements_mobile'>
+        <div className='logo_calu'>
+          <img src={calu_logo} alt='calu' />
           <p>TU AGENCIA DE MARKETING DIGITAL</p>
         </div>
 
-        <div className="servicios">
+        <div className='servicios'>
           <h1>SERVICIOS</h1>
 
-          <div className="servicios_">
+          <div className='servicios_'>
             <ul>
               <li>
                 <span>Consultoría Estratégica</span>
@@ -148,38 +165,29 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="redes">
+        <div className='redes'>
           <h1>REDES</h1>
-          <div className="social_icons">
-            <a
-              href="https://www.linkedin.com/company/calu-mktdigital/"
-              target="_blank"
-            >
-              <img src={ld_logo} alt="linkedin" />
+          <div className='social_icons'>
+            <a href='https://www.linkedin.com/company/calu-mktdigital/' target='_blank'>
+              <img src={ld_logo} alt='linkedin' />
+            </a>
+            <a href='https://www.facebook.com/profile.php?id=100090611090874' target='_blank'>
+              <img src={fb_logo} alt='facebook' />
+            </a>
+            <a href='https://www.instagram.com/calu.mktdigital/'>
+              <img src={insta_logo} alt='instagram' />
             </a>
             <a
-              href="https://www.facebook.com/profile.php?id=100090611090874"
-              target="_blank"
+              href='https://www.tiktok.com/@calumktdigital?is_from_webapp=1&sender_device=pc'
+              target='_blank'
             >
-              <img src={fb_logo} alt="facebook" />
+              <img src={tk_logo} alt='tiktok' />
             </a>
-            <a href="https://www.instagram.com/calu.mktdigital/">
-              <img src={insta_logo} alt="instagram" />
+            <a href='https://open.spotify.com/user/31qdqqxbhaph6tshdgkoidsug6ae' target='_blank'>
+              <img src={sp_logo} alt='spotify' />
             </a>
-            <a
-              href="https://www.tiktok.com/@calumktdigital?is_from_webapp=1&sender_device=pc"
-              target="_blank"
-            >
-              <img src={tk_logo} alt="tiktok" />
-            </a>
-            <a
-              href="https://open.spotify.com/user/31qdqqxbhaph6tshdgkoidsug6ae"
-              target="_blank"
-            >
-              <img src={sp_logo} alt="spotify" />
-            </a>
-            <a href="https://youtube.com/@CaluMktdigital" target="_blank">
-              <img src={yt_logo} alt="youtube" />
+            <a href='https://youtube.com/@CaluMktdigital' target='_blank'>
+              <img src={yt_logo} alt='youtube' />
             </a>
           </div>
         </div>
