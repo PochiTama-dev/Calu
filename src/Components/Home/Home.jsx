0b14cc952c9c44e30cfb8e3cc563a8dfa83d25eb
facuzero <facuzero_95@hotmail.com>
2023-11-01 12:35:10 -0300

    observaciones

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,33 +1,31 @@
-import { useState, useRef } from "react";
-import "animate.css/animate.min.css";
-import "./home.css";
-import { Header } from "../Header/header";
-import Footer from "../Footer/Footer";
-import Portfolio from "../Portfolio/Portfolio";
-import About from "../About/About";
-import News from "../News/News";
-import Onboarding from "../Onboarding/Onboarding";
-import OurServices from "../OurServices/OurServices";
-import CTN from "../CTN/CTN";
-import Contact_button from "./Contact_button/Contact_button";
-import React from "react";
-import arrow_L from "./icon_arrow_left.svg";
-import Resources from "../Resources/Resources";
-import { useCustomContext } from "../../Hooks/Context/Context";
+import { useState, useRef } from 'react';
+import 'animate.css/animate.min.css';
+import './home.css';
+import { Header } from '../Header/header';
+import Footer from '../Footer/Footer';
+import Portfolio from '../Portfolio/Portfolio';
+import About from '../About/About';
+import News from '../News/News';
+import Onboarding from '../Onboarding/Onboarding';
+import OurServices from '../OurServices/OurServices';
+import CTN from '../CTN/CTN';
+import Contact_button from './Contact_button/Contact_button';
+import React from 'react';
+import arrow_L from './icon_arrow_left.svg';
+import Resources from '../Resources/Resources';
+import { useCustomContext } from '../../Hooks/Context/Context';
 const Home = () => {
   const { cart, removeFromCart } = useCustomContext();
   const numSections = 5;
   const [currentSection, setCurrentSection] = useState(0);
 
   const sectionStyles = {
-    transition: "transform 0.5s ease-in-out",
+    transition: 'transform 0.5s ease-in-out',
   };
 
   const getSectionClassName = (sectionNumber) =>
     `section-${sectionNumber} ${
-      currentSection === sectionNumber
-        ? "animate__animated animate__fadeIn"
-        : ""
+      currentSection === sectionNumber ? 'animate__animated animate__fadeIn' : ''
     }`;
 
   const [width, setWidth] = React.useState(window.innerWidth);
@@ -35,31 +33,27 @@ const Home = () => {
   React.useEffect(() => {
     const handleResizeWindow = () => setWidth(window.innerWidth);
 
-    window.addEventListener("resize", handleResizeWindow);
+    window.addEventListener('resize', handleResizeWindow);
     return () => {
-      window.removeEventListener("resize", handleResizeWindow);
+      window.removeEventListener('resize', handleResizeWindow);
     };
   }, []);
   //////////// Scroll to top
   const firstSection = useRef(null);
   const scrollToTop = () => {
-    firstSection.current?.scrollIntoView({ behavior: "smooth" });
+    firstSection.current?.scrollIntoView({ behavior: 'smooth' });
   };
   //////////////
   if (width > breakpoint) {
     return (
-      <div className="container">
+      <div className='container'>
         <Header cartItem={cart} handleDelete={removeFromCart} />
         <button onClick={scrollToTop}>
-          <img className="arrow_up" src={arrow_L} />
+          <img className='arrow_up' src={arrow_L} />
         </button>
         <Contact_button />
 
-        <section
-          style={sectionStyles}
-          className={getSectionClassName(0)}
-          ref={firstSection}
-        >
+        <section style={sectionStyles} className={getSectionClassName(0)} ref={firstSection}>
           <Onboarding />
         </section>
         <section style={sectionStyles} className={getSectionClassName(1)}>
@@ -85,17 +79,13 @@ const Home = () => {
     );
   }
   return (
-    <div className="container">
+    <div className='container'>
       <Header cartItem={cart} />
       <button onClick={scrollToTop}>
-        <img className="arrow_up" src={arrow_L} />
+        <img className='arrow_up' src={arrow_L} />
       </button>
       <Contact_button />
-      <section
-        ref={firstSection}
-        style={sectionStyles}
-        className={getSectionClassName(0)}
-      >
+      <section ref={firstSection} style={sectionStyles} className={getSectionClassName(0)}>
         <Onboarding />
       </section>
       <section style={sectionStyles} className={getSectionClassName(1)}>
@@ -116,10 +106,7 @@ const Home = () => {
       <section style={sectionStyles} className={getSectionClassName(5)}>
         <CTN />
       </section>
-      <section
-        style={sectionStyles}
-        className={getSectionClassName(6)}
-      ></section>
+      <section style={sectionStyles} className={getSectionClassName(6)}></section>
       <Footer />
     </div>
   );
